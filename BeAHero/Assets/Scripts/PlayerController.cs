5e51227b6ae2d1f7f3e1d4ccbb4173e08400c8e3
IshidaMasatoshi <m.ishida@Apple-no-MacBook-Pro.local>
2018-01-10 23:47:41 +0900

    キャラクター設置。タッチ箇所に移動。

---
+++ BeAHero/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+//		if(Input.touchCount > 0){ // Editor/マウス操作の場合は Input.GetMouseButton(0) にする
+		if(Input.GetMouseButton(0)){
+			var pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			pos.z = 0;
+			transform.position = pos;
+		}
+	}
+}
